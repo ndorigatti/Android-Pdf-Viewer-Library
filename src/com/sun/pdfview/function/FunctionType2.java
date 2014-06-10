af9567a6528864edf77d8b4dc79fb2aca01cc074
Nicola Dorigatti <turricatch@gmail.com>
2014-06-10 21:48:56 +0200

    PDFFunction improvements
    Increased PDFFunction supported features

--- src/com/sun/pdfview/function/FunctionType2.java
+++ src/com/sun/pdfview/function/FunctionType2.java
@@ -22,7 +22,6 @@
 package com.sun.pdfview.function;
 
 import java.io.IOException;
-
 import com.sun.pdfview.PDFObject;
 import com.sun.pdfview.PDFParseException;
 
@@ -32,114 +31,116 @@ import com.sun.pdfview.PDFParseException;
  * formula.
  */
 public class FunctionType2 extends PDFFunction {
-    /** the function's value at zero for the n outputs */
-    private float[] c0 = new float[] { 0f };
-    
-    /** the function's value at one for the n outputs */
-    private float[] c1 = new float[] { 1f };
-    
-    /** the exponent */
-    private float n;
-    
-    /** Creates a new instance of FunctionType2 */
-    public FunctionType2() {
-        super(TYPE_2);
-    }
-    
-    /**
-     * Read the zeros, ones and exponent
-     */
-    @Override
-	protected void parse(PDFObject obj) throws IOException 
-    {
-        // read the exponent (required)
-        PDFObject nObj = obj.getDictRef("N");
-        if (nObj == null) {
-            throw new PDFParseException("Exponent required for function type 2!");
-        }
-        setN(nObj.getFloatValue());
-        
-        // read the zeros array (optional)
-        PDFObject cZeroObj = obj.getDictRef("C0");
-        if (cZeroObj != null) {
-            PDFObject[] cZeroAry = cZeroObj.getArray();
-            float[] cZero = new float[cZeroAry.length];
-            for (int i = 0; i < cZeroAry.length; i++) {
-                cZero[i] = cZeroAry[i].getFloatValue();
-            }
-            setC0(cZero);
-        }
-        
-        // read the ones array (optional)
-        PDFObject cOneObj = obj.getDictRef("C1");
-        if (cOneObj != null) {
-            PDFObject[] cOneAry = cOneObj.getArray();
-            float[] cOne = new float[cOneAry.length];
-            for (int i = 0; i < cOneAry.length; i++) {
-                cOne[i] = cOneAry[i].getFloatValue();
-            }
-            setC1(cOne);
-        }
-    }
-    
-    /**
-     * Calculate the function value for the input.  For each output (j),
-     * the function value is:
-     * C0(j) + x^N * (C1(j) - C0(j))
-     */
-    @Override
-	protected void doFunction(float[] inputs, int inputOffset, 
-                              float[] outputs, int outputOffset)
-    {
-        // read the input value
-        float input = inputs[inputOffset];
-        
-        // calculate the output values
-        for (int i = 0; i < getNumOutputs(); i++) {
-            outputs[i + outputOffset] = getC0(i) + 
-                (float) (Math.pow(input, getN()) * (getC1(i) - getC0(i)));
-        }
-    }
-    
-    /**
-     * Get the exponent
-     */
-    public float getN() {
-        return n;
-    }
-    
-    /**
-     * Set the exponent
-     */
-    protected void setN(float n) {
-        this.n = n;
-    }
-    
-    /**
-     * Get the values at zero
-     */
-    public float getC0(int index) {
-        return c0[index];
-    }
-    
-    /**
-     * Set the values at zero
-     */
-    protected void setC0(float[] c0) {
-        this.c0 = c0;
-    }
-    
-    /**
-     * Get the values at one
-     */
-    public float getC1(int index) {
-        return c1[index];
-    }
-    
-    /**
-     * Set the values at one
-     */
-    protected void setC1(float[] c1) {
-        this.c1 = c1;
-    }  
+	/** the function's value at zero for the n outputs */
+	private float[] c0 = new float[] { 0f };
+
+	/** the function's value at one for the n outputs */
+	private float[] c1 = new float[] { 1f };
+
+	/** the exponent */
+	private float n;
+
+	/** Creates a new instance of FunctionType2 */
+	public FunctionType2() {
+		super(TYPE_2);
+	}
+
+	/**
+	 * Read the zeros, ones and exponent
+	 */
+	@Override
+	protected void parse(PDFObject obj) throws IOException {
+		// read the exponent (required)
+		PDFObject nObj = obj.getDictRef("N");
+		if (nObj == null) {
+			throw new PDFParseException("Exponent required for function type 2!");
+		}
+		setN(nObj.getFloatValue());
+
+		// read the zeros array (optional)
+		PDFObject cZeroObj = obj.getDictRef("C0");
+		if (cZeroObj != null) {
+			PDFObject[] cZeroAry = cZeroObj.getArray();
+			float[] cZero = new float[cZeroAry.length];
+			for (int i = 0; i < cZeroAry.length; i++) {
+				cZero[i] = cZeroAry[i].getFloatValue();
+			}
+			setC0(cZero);
+		}
+
+		// read the ones array (optional)
+		PDFObject cOneObj = obj.getDictRef("C1");
+		if (cOneObj != null) {
+			PDFObject[] cOneAry = cOneObj.getArray();
+			float[] cOne = new float[cOneAry.length];
+			for (int i = 0; i < cOneAry.length; i++) {
+				cOne[i] = cOneAry[i].getFloatValue();
+			}
+			setC1(cOne);
+		}
+	}
+
+	/**
+	 * Calculate the function value for the input. For each output (j),
+	 * the function value is:
+	 * C0(j) + x^N * (C1(j) - C0(j))
+	 */
+	@Override
+	protected void doFunction(float[] inputs, int inputOffset, float[] outputs, int outputOffset) {
+		// read the input value
+		float input = inputs[inputOffset];
+
+		// calculate the output values
+		for (int i = 0; i < getNumOutputs(); i++) {
+			outputs[i + outputOffset] = getC0(i) + (float) (Math.pow(input, getN()) * (getC1(i) - getC0(i)));
+		}
+	}
+
+	public int getNumOutputs() {
+		// range can be null (only required for type 0 and type 4 functions),
+		// but c0 is required
+		return c0.length;
+	}
+
+	/**
+	 * Get the exponent
+	 */
+	public float getN() {
+		return n;
+	}
+
+	/**
+	 * Set the exponent
+	 */
+	protected void setN(float n) {
+		this.n = n;
+	}
+
+	/**
+	 * Get the values at zero
+	 */
+	public float getC0(int index) {
+		return c0[index];
+	}
+
+	/**
+	 * Set the values at zero
+	 */
+	protected void setC0(float[] c0) {
+		this.c0 = c0;
+	}
+
+	/**
+	 * Get the values at one
+	 */
+	public float getC1(int index) {
+		return c1[index];
+	}
+
+	/**
+	 * Set the values at one
+	 */
+	protected void setC1(float[] c1) {
+		this.c1 = c1;
+	}
 }
